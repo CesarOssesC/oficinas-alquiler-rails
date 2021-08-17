6ecb2c84ddf38d2e38528cb36b7f33797a869a60
César Osses <cesar.osses@gmail.com>
2021-08-17 15:14:06 -0400

    Termino dia 1. Creacion tabla oficinas y validaciones

---
+++ app/controllers/oficinas_controller.rb
@@ -0,0 +1,27 @@
+class OficinasController < ApplicationController
+  
+  #get
+  def index
+    @lista_oficinas = Oficina.all.order(id: :asc)
+  end
+
+  #get
+  def crear
+    @oficina_nueva = Oficina.new
+  end
+
+  #post
+  def guardar
+    valores = params.require(:oficina).permit(:codigo, :precio, :esta_alquilada)
+
+    @oficina_nueva = Oficina.new(valores)
+    
+    if @oficina_nueva.save
+      redirect_to action: "index"
+    else  
+      render :crear
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'oficinas/index', to: 'oficinas#index'
+  get 'oficinas/crear', to: 'oficinas#crear'
+  post 'oficinas/guardar', to: 'oficinas#guardar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
